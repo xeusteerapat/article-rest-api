783953bcc3c711632030bff174a132e9ecb99721
xeusteerapat <xeus085@gmail.com>
2024-04-29 00:23:20 +0700

    🐛 [fix] fix import deps

--- src/main/kotlin/com/xeus/restApiAuthDemo/RestApiAuthDemoApplication.kt
+++ src/main/kotlin/com/xeus/restApiAuthDemo/RestApiAuthDemoApplication.kt
@@ -2,6 +2,9 @@ package com.xeus.restApiAuthDemo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class RestApiAuthDemoApplication
