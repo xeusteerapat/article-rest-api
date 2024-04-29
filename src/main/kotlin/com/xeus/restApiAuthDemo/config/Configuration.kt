d93b242c6d89b4ae3cff42e4370941e8e19157f2
xeusteerapat <xeus085@gmail.com>
2024-04-29 21:42:08 +0700

    🔒️ [feat] add jwt token

---
+++ src/main/kotlin/com/xeus/restApiAuthDemo/config/Configuration.kt
@@ -0,0 +1,9 @@
+package com.xeus.restApiAuthDemo.config
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableConfigurationProperties(JwtProperties::class)
+class Configuration {
+}
