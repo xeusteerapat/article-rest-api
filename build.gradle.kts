325c1ee4a46c71e4a8e7f30492baf51c70001df7
xeusteerapat <xeus085@gmail.com>
2024-04-29 21:03:25 +0700

    ➕ [feat] add jwt deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
+
+	implementation("org.springframework.boot:spring-boot-starter-security")
+
+	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
